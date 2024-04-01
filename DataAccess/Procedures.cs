3a791cd0b20f1082b4bef5ea010036a28b614c99
Pratap-Lap <app@formalighting.com>
2024-04-02 01:27:10 +0530

    added job service

---
+++ DataAccess/Procedures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class Procedures
+    {
+        public static class Jobs
+        {
+            public const string USP_GETJOBS = "USP_GetJobs";
+        }
+    }
+}
---
+++ MaxTransApi/Controllers/JobController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess;
+using MaxTransApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaxTransApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getjobs")]
+        public async Task<IActionResult> GetJobs(string? jobStatus, string? createdBy)
+        {
+            try {
+                var jobs = new JobService().GetJob(jobStatus, createdBy);
+                return Ok(new ApiResult
+                {
+                    Data = jobs,
+                    IsSuccess = true
+                });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new ApiResult
+                {
+                    Data = null,
+                    IsSuccess = false,
+                    Message = ex.Message
+                });
+            }
+            
+        }
+    }
+}
