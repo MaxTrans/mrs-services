5de1759278c5b79c0cd91f2eef34137e52962f6a
Pratap-Lap <app@formalighting.com>
2024-05-02 09:35:50 +0530

    job delete sp

---
+++ MaxTransDB/dbo/Stored Procedures/usp_DeleteJob.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE usp_DeleteJob
+(
+	@JobId UNIQUEIDENTIFIER,
+	@UserId UNIQUEIDENTIFIER
+)
+AS
+BEGIN
+	UPDATE Jobs SET IsDeleted = 1,ModifyedBy = @UserId, ModifiedDateTime = GETUTCDATE()
+	WHERE Id = @JobId
+END
