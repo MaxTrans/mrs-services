39be25cbde3b0f417a54037f98b3874b1835c444
pradeep <i.pradeepkumar.mca@gmail.com>
2024-05-04 00:25:39 +0530

    page count update DB changes

---
+++ MaxTransDB/dbo/Stored Procedures/usp_UpdatePageCount.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[usp_UpdatePageCount]
+	@FileXml XML
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	;WITH ParsedData AS (
+		SELECT 
+			Item.value('(@id)[1]', 'VARCHAR(500)') AS Id,
+			Item.value('(@pageCount)[1]', 'INT') as [PageCount]
+		FROM @FileXml.nodes('/Files/FileInfo') AS X(Item))
+
+	UPDATE JF SET JF.[PageCount] = pd.[PageCount]
+	FROM JobFiles JF
+	JOIN ParsedData PD ON JF.FileId = PD.Id
+	WHERE JF.FileId = PD.Id
+END
